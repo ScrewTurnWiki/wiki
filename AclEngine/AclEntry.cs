f556ac53c96ca04fa3e9e5938f45600da3ab492c
dodexahedron <brandon@dodecatec.com>
2014-07-23 17:13:32 -0400

    The equality comparison was broken.  The only reachable code path was the return x.Equals(y) line at the end.  Reference comparison seems to have been intended for the first few lines.

--- AclEngine/AclEntry.cs
+++ AclEngine/AclEntry.cs
@@ -6,29 +6,47 @@ namespace ScrewTurn.Wiki.AclEngine {
 	/// <summary>
 	/// Represents an ACL Entry.
 	/// </summary>
-	public class AclEntry {
+	public class AclEntry : IEquatable<AclEntry>
+	{
+		public override int GetHashCode( )
+		{
+			unchecked
+			{
+				int hashCode = ( Resource != null ? Resource.GetHashCode( ) : 0 );
+				hashCode = ( hashCode * 397 ) ^ ( Action != null ? Action.GetHashCode( ) : 0 );
+				hashCode = ( hashCode * 397 ) ^ ( Subject != null ? Subject.GetHashCode( ) : 0 );
+				hashCode = ( hashCode * 397 ) ^ (int) Value;
+				return hashCode;
+			}
+		}
+
+
+		/// <summary>
+		/// Determines if two AclEntry objects are equal, by value.
+		/// </summary>
+		/// <param name="left"></param>
+		/// <param name="right"></param>
+		/// <returns></returns>
+		public static bool operator ==( AclEntry left, AclEntry right )
+		{
+			return Equals( left, right );
+		}
+
+		/// <summary>
+		/// Determines if two AclEntry objects are unequal, by value.
+		/// </summary>
+		/// <param name="left"></param>
+		/// <param name="right"></param>
+		/// <returns></returns>
+		public static bool operator !=( AclEntry left, AclEntry right )
+		{
+			return !Equals( left, right );
+		}
 
 		/// <summary>
 		/// The full control action.
 		/// </summary>
 		public const string FullControlAction = "*";
-
-		/// <summary>
-		/// The controlled resource.
-		/// </summary>
-		private string resource;
-		/// <summary>
-		/// The controlled action on the resource.
-		/// </summary>
-		private string action;
-		/// <summary>
-		/// The subject whose access to the resource/action is controlled.
-		/// </summary>
-		private string subject;
-		/// <summary>
-		/// The entry value.
-		/// </summary>
-		private Value value;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="T:AclEntry" /> class.
@@ -39,62 +57,53 @@ namespace ScrewTurn.Wiki.AclEngine {
 		/// <param name="value">The entry value.</param>
 		/// <exception cref="ArgumentNullException">If <paramref name="resource"/>, <paramref name="action"/> or <paramref name="subject"/> are <c>null</c>.</exception>
 		/// <exception cref="ArgumentException">If <paramref name="resource"/>, <paramref name="action"/> or <paramref name="subject"/> are empty.</exception>
-		public AclEntry(string resource, string action, string subject, Value value) {
-			if(resource == null) throw new ArgumentNullException("resource");
-			if(resource.Length == 0) throw new ArgumentException("Resource cannot be empty", "resource");
-			if(action == null) throw new ArgumentNullException("action");
-			if(action.Length == 0) throw new ArgumentException("Action cannot be empty", "action");
-			if(subject == null) throw new ArgumentNullException("subject");
-			if(subject.Length == 0) throw new ArgumentException("Subject cannot be empty", "subject");
+		public AclEntry(string resource, string action, string subject, Value value)
+		{
+			if ( resource == null )
+				throw new ArgumentNullException( "resource" );
+			if ( resource.Length == 0 )
+				throw new ArgumentException( "Resource cannot be empty", "resource" );
+			if ( action == null )
+				throw new ArgumentNullException( "action" );
+			if ( action.Length == 0 )
+				throw new ArgumentException( "Action cannot be empty", "action" );
+			if ( subject == null )
+				throw new ArgumentNullException( "subject" );
+			if ( subject.Length == 0 )
+				throw new ArgumentException( "Subject cannot be empty", "subject" );
 
-			this.resource = resource;
-			this.action = action;
-			this.subject = subject;
-			this.value = value;
+			Resource = resource;
+			Action = action;
+			Subject = subject;
+			Value = value;
 		}
 
 		/// <summary>
 		/// Gets the controlled resource.
 		/// </summary>
-		public string Resource {
-			get { return resource; }
-		}
+		public string Resource { get; private set; }
 
 		/// <summary>
 		/// Gets the controlled action on the resource.
 		/// </summary>
-		public string Action {
-			get { return action; }
-		}
+		public string Action { get; private set; }
 
 		/// <summary>
 		/// Gets the subject of the entry.
 		/// </summary>
-		public string Subject {
-			get { return subject; }
-		}
+		public string Subject { get; private set; }
 
 		/// <summary>
 		/// Gets the value of the entry.
 		/// </summary>
-		public Value Value {
-			get { return value; }
-		}
+		public Value Value { get; private set; }
 
 		/// <summary>
 		/// Gets a string representation of the current object.
 		/// </summary>
 		/// <returns>The string representation.</returns>
 		public override string ToString() {
-			return resource + "->" + action + ": " + subject + " (" + value + ")";
-		}
-
-		/// <summary>
-		/// Gets a hash code for the current object.
-		/// </summary>
-		/// <returns>The hash code.</returns>
-		public override int GetHashCode() {
-			return base.GetHashCode();
+			return Resource + "->" + Action + ": " + Subject + " (" + Value + ")";
 		}
 
 		/// <summary>
@@ -102,10 +111,21 @@ namespace ScrewTurn.Wiki.AclEngine {
 		/// </summary>
 		/// <param name="obj">The other object.</param>
 		/// <returns><c>true</c> if this object equals <b>obj</b>, <c>false</c> otherwise.</returns>
-		public override bool Equals(object obj) {
-			AclEntry other = obj as AclEntry;
-			if(other != null) return Equals(other);
-			return false;
+		public override bool Equals( object obj )
+		{
+			if ( ReferenceEquals( null, obj ) )
+			{
+				return false;
+			}
+			if ( ReferenceEquals( this, obj ) )
+			{
+				return true;
+			}
+			if ( obj.GetType( ) != GetType( ) )
+			{
+				return false;
+			}
+			return Equals( (AclEntry) obj );
 		}
 
 		/// <summary>
@@ -113,11 +133,20 @@ namespace ScrewTurn.Wiki.AclEngine {
 		/// </summary>
 		/// <param name="other">The other instance.</param>
 		/// <returns><c>true</c> if this instance equals <b>other</b>, <c>false</c> otherwise.</returns>
-		public bool Equals(AclEntry other)
+		public bool Equals( AclEntry other )
 		{
-			if(object.ReferenceEquals(other, null)) return false;
-			return resource == other.Resource &&
-			       action == other.Action && subject == other.Subject;
+			if ( ReferenceEquals( null, other ) )
+			{
+				return false;
+			}
+			if ( ReferenceEquals( this, other ) )
+			{
+				return true;
+			}
+			return string.Equals( Resource, other.Resource )
+				&& string.Equals( Action, other.Action )
+				&& string.Equals( Subject, other.Subject )
+				&& Value == other.Value;
 		}
 
 		/// <summary>
@@ -126,11 +155,15 @@ namespace ScrewTurn.Wiki.AclEngine {
 		/// <param name="x">The first instance.</param>
 		/// <param name="y">The second instance.</param>
 		/// <returns><c>true</c> if <b>x</b> equals <b>y</b>, <c>false</c> otherwise.</returns>
-		public static bool Equals(AclEntry x, AclEntry y) {
-			if(object.ReferenceEquals(x, null) && !object.ReferenceEquals(x, null)) return false;
-			if(!object.ReferenceEquals(x, null) && object.ReferenceEquals(x, null)) return false;
-			if(object.ReferenceEquals(x, null) && object.ReferenceEquals(x, null)) return true;
-			return x.Equals(y);
+		public static bool Equals( AclEntry x, AclEntry y )
+		{
+			if ( ReferenceEquals( x, null ) && !ReferenceEquals( y, null ) )
+				return false;
+			if ( ReferenceEquals( x, null ) )
+				return true;
+			if ( ReferenceEquals( y, null ) )
+				return false;
+			return x.Equals( y );
 		}
 
 	}
