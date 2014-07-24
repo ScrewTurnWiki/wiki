d90134c171642b093f2b2aae57916d47797af5e5
dodexahedron <brandon@dodecatec.com>
2014-07-24 15:54:34 -0400

    Remove all the explicit namespace qualifiers.

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -5,8 +5,9 @@ using System.Web;
 namespace ScrewTurn.Wiki
 {
 	using System.IO;
+	using ScrewTurn.Wiki.PluginFramework;
 
-	public class Global : System.Web.HttpApplication
+	public class Global : HttpApplication
 	{
 
 		protected void Application_Start( object sender, EventArgs e )
@@ -18,7 +19,7 @@ namespace ScrewTurn.Wiki
 		{
 			// Increment # of online users and setup a new breadcrumbs manager
 			// TODO: avoid to increment # of online users when session is not InProc
-			ScrewTurn.Wiki.Cache.OnlineUsers++;
+			Cache.OnlineUsers++;
 		}
 
 		protected void Application_BeginRequest( object sender, EventArgs e )
@@ -29,8 +30,8 @@ namespace ScrewTurn.Wiki
 				if ( Application[ "StartupOK" ] == null )
 				{
 					// Setup Resource Exchanger
-					ScrewTurn.Wiki.Exchanger.ResourceExchanger = new ScrewTurn.Wiki.ResourceExchanger( );
-					ScrewTurn.Wiki.StartupTools.Startup( );
+					Exchanger.ResourceExchanger = new ResourceExchanger( );
+					StartupTools.Startup( );
 
 					// All is OK, proceed with normal startup operations
 					Application[ "StartupOK" ] = "OK";
@@ -49,7 +50,7 @@ namespace ScrewTurn.Wiki
 				SessionCache.ClearData( HttpContext.Current.Session.SessionID );
 
 				// Try to automatically login the user through the cookie
-				ScrewTurn.Wiki.LoginTools.TryAutoLogin( );
+				LoginTools.TryAutoLogin( );
 			}
 		}
 
@@ -67,9 +68,9 @@ namespace ScrewTurn.Wiki
 			//if(ex.InnerException != null) ex = ex.InnerException;
 			try
 			{
-				ScrewTurn.Wiki.Log.LogEntry( Tools.GetCurrentUrlFixed( ) + "\n" +
+				Log.LogEntry( Tools.GetCurrentUrlFixed( ) + "\n" +
 					ex.Source + " thrown " + ex.GetType( ).FullName + "\n" + ex.Message + "\n" + ex.StackTrace,
-					ScrewTurn.Wiki.PluginFramework.EntryType.Error, ScrewTurn.Wiki.Log.SystemUsername );
+					EntryType.Error, Log.SystemUsername );
 			}
 			catch { }
 		}
@@ -86,8 +87,8 @@ namespace ScrewTurn.Wiki
 				// Try to redirect an inexistent .aspx page to a probably existing .ashx page
 				if ( httpEx.GetHttpCode( ) == 404 )
 				{
-					string page = System.IO.Path.GetFileNameWithoutExtension( Request.PhysicalPath );
-					ScrewTurn.Wiki.UrlTools.Redirect( page + ScrewTurn.Wiki.Settings.PageExtension );
+					string page = Path.GetFileNameWithoutExtension( Request.PhysicalPath );
+					UrlTools.Redirect( page + Settings.PageExtension );
 					return;
 				}
 			}
@@ -101,19 +102,19 @@ namespace ScrewTurn.Wiki
 			catch { }
 			EmailTools.NotifyError( ex, url );
 			Session[ "LastError" ] = Server.GetLastError( );
-			if ( !Request.PhysicalPath.ToLowerInvariant( ).Contains( "error.aspx" ) ) ScrewTurn.Wiki.UrlTools.Redirect( "Error.aspx" );
+			if ( !Request.PhysicalPath.ToLowerInvariant( ).Contains( "error.aspx" ) ) UrlTools.Redirect( "Error.aspx" );
 		}
 
 		protected void Session_End( object sender, EventArgs e )
 		{
 			// Decrement # of online users (only works when session is InProc)
-			ScrewTurn.Wiki.Cache.OnlineUsers--;
+			Cache.OnlineUsers--;
 		}
 
 		protected void Application_End( object sender, EventArgs e )
 		{
 			// Try to cleanly shutdown the application and providers
-			ScrewTurn.Wiki.StartupTools.Shutdown( );
+			StartupTools.Shutdown( );
 		}
 
 		/// <summary>
@@ -121,7 +122,7 @@ namespace ScrewTurn.Wiki
 		/// </summary>
 		public static void RouteCurrentRequest( )
 		{
-			string physicalPath = null;
+			string physicalPath;
 
 			try
 			{
