77b5030c297238e670f39b64196c943488cb0572
dodexahedron <brandon@dodecatec.com>
2014-07-25 00:43:22 -0400

    Fixed another bad exception.
    Minor cleanup.

--- AclEngine/AclChangedEventArgs.cs
+++ AclEngine/AclChangedEventArgs.cs
@@ -1,16 +1,13 @@
-﻿
-using System;
+﻿using System;
 
-namespace ScrewTurn.Wiki.AclEngine {
+namespace ScrewTurn.Wiki.AclEngine
+{
 
 	/// <summary>
 	/// Contains arguments for the <see cref="IAclManager.AclChanged" /> event.
 	/// </summary>
-	public class AclChangedEventArgs : EventArgs {
-
-		private AclEntry[] entries;
-		private Change change;
-
+	public class AclChangedEventArgs : EventArgs
+	{
 		/// <summary>
 		/// Initializes a new instance of the <see cref="T:AclChangedEventArgs" /> class.
 		/// </summary>
@@ -18,34 +15,33 @@ namespace ScrewTurn.Wiki.AclEngine {
 		/// <param name="change">The change.</param>
 		/// <exception cref="ArgumentNullException">If <paramref name="entries"/> is <c>null</c>.</exception>
 		/// <exception cref="ArgumentException">If <paramref name="entries"/> is empty.</exception>
-		public AclChangedEventArgs(AclEntry[] entries, Change change) {
-			if(entries == null) throw new ArgumentNullException("entry");
-			if(entries.Length == 0) throw new ArgumentException("Entries cannot be empty", "entries");
+		public AclChangedEventArgs( AclEntry[ ] entries, Change change )
+		{
+			if ( entries == null )
+				throw new ArgumentNullException( "entries" );
+			if ( entries.Length == 0 )
+				throw new ArgumentException( "Entries cannot be empty", "entries" );
 
-			this.entries = entries;
-			this.change = change;
+			Entries = entries;
+			Change = change;
 		}
 
 		/// <summary>
 		/// Gets the entries that changed.
 		/// </summary>
-		public AclEntry[] Entries {
-			get { return entries; }
-		}
+		public AclEntry[ ] Entries { get; private set; }
 
 		/// <summary>
 		/// Gets the change.
 		/// </summary>
-		public Change Change {
-			get { return change; }
-		}
-
+		public Change Change { get; private set; }
 	}
 
 	/// <summary>
 	/// Lists legal changes for ACL entries.
 	/// </summary>
-	public enum Change {
+	public enum Change
+	{
 		/// <summary>
 		/// An entry is stored.
 		/// </summary>
