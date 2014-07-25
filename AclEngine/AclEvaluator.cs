d7885cfbc05ded42078063ef22ed11c9923afecd
dodexahedron <brandon@dodecatec.com>
2014-07-25 00:50:20 -0400

    Whitespace changes only

--- AclEngine/AclEvaluator.cs
+++ AclEngine/AclEvaluator.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Collections.Generic;
 
-namespace ScrewTurn.Wiki.AclEngine {
+namespace ScrewTurn.Wiki.AclEngine
+{
 
 	/// <summary>
 	/// Implements tools for evaluating permissions.
 	/// </summary>
-	public static class AclEvaluator {
+	public static class AclEvaluator
+	{
 
 		/// <summary>
 		/// Decides whether a user, member of some groups, is authorized to perform an action on a resource.
@@ -20,72 +22,96 @@ namespace ScrewTurn.Wiki.AclEngine {
 		/// <returns>The positive, negative, or indeterminate result.</returns>
 		/// <exception cref="ArgumentNullException">If <paramref name="resource"/>, <paramref name="action"/>, <paramref name="user"/>, <paramref name="groups"/> or <paramref name="entries"/> are <c>null</c>.</exception>
 		/// <exception cref="ArgumentException">If <paramref name="resource"/>, <paramref name="action"/>, <paramref name="user"/> are empty, or if <paramref name="action"/> equals <see cref="AclEntry.FullControlAction"/>.</exception>
-		public static Authorization AuthorizeAction(string resource, string action, string user, string[] groups, AclEntry[] entries) {
-			if(resource == null) throw new ArgumentNullException("resource");
-			if(resource.Length == 0) throw new ArgumentException("Resource cannot be empty", "resource");
-			if(action == null) throw new ArgumentNullException("action");
-			if(action.Length == 0) throw new ArgumentException("Action cannot be empty", "action");
-			if(action == AclEntry.FullControlAction) throw new ArgumentException("Action cannot be the FullControl flag", "action");
-			if(user == null) throw new ArgumentNullException("user");
-			if(user.Length == 0) throw new ArgumentException("User cannot be empty", "user");
-			if(groups == null) throw new ArgumentNullException("groups");
-			if(entries == null) throw new ArgumentNullException("entries");
+		public static Authorization AuthorizeAction( string resource, string action, string user, string[ ] groups, AclEntry[ ] entries )
+		{
+			if ( resource == null )
+				throw new ArgumentNullException( "resource" );
+			if ( resource.Length == 0 )
+				throw new ArgumentException( "Resource cannot be empty", "resource" );
+			if ( action == null )
+				throw new ArgumentNullException( "action" );
+			if ( action.Length == 0 )
+				throw new ArgumentException( "Action cannot be empty", "action" );
+			if ( action == AclEntry.FullControlAction )
+				throw new ArgumentException( "Action cannot be the FullControl flag", "action" );
+			if ( user == null )
+				throw new ArgumentNullException( "user" );
+			if ( user.Length == 0 )
+				throw new ArgumentException( "User cannot be empty", "user" );
+			if ( groups == null )
+				throw new ArgumentNullException( "groups" );
+			if ( entries == null )
+				throw new ArgumentNullException( "entries" );
 
 			// Simple ACL model
 			// Sort entries so that FullControl ones are at the bottom
 			// First look for an entry specific for the user
 			// If not found, look for a group that denies the permission
 
-			AclEntry[] sortedEntries = new AclEntry[entries.Length];
-			Array.Copy(entries, sortedEntries, entries.Length);
+			AclEntry[ ] sortedEntries = new AclEntry[ entries.Length ];
+			Array.Copy( entries, sortedEntries, entries.Length );
 
-			Array.Sort(sortedEntries, delegate(AclEntry x, AclEntry y) {
-				return x.Action.CompareTo(y.Action);
-			});
-			Array.Reverse(sortedEntries);
+			Array.Sort( sortedEntries, delegate( AclEntry x, AclEntry y )
+			{
+				return x.Action.CompareTo( y.Action );
+			} );
+			Array.Reverse( sortedEntries );
 
-			foreach(AclEntry entry in sortedEntries) {
-				if(entry.Resource == resource && (entry.Action == action || entry.Action == AclEntry.FullControlAction) && entry.Subject == user)
+			foreach ( AclEntry entry in sortedEntries )
+			{
+				if ( entry.Resource == resource && ( entry.Action == action || entry.Action == AclEntry.FullControlAction ) && entry.Subject == user )
 				{
-					if(entry.Value == Value.Grant) return Authorization.Granted;
-					if(entry.Value == Value.Deny) return Authorization.Denied;
-					throw new NotSupportedException("Entry value not supported");
+					if ( entry.Value == Value.Grant )
+						return Authorization.Granted;
+					if ( entry.Value == Value.Deny )
+						return Authorization.Denied;
+					throw new NotSupportedException( "Entry value not supported" );
 				}
 			}
 
 			// For each group, a decision is made
-			Dictionary<string, bool> groupFullControlGrant = new Dictionary<string, bool>();
-			Dictionary<string, bool> groupExplicitGrant = new Dictionary<string, bool>();
-			Dictionary<string, bool> groupFullControlDeny = new Dictionary<string, bool>();
+			Dictionary<string, bool> groupFullControlGrant = new Dictionary<string, bool>( );
+			Dictionary<string, bool> groupExplicitGrant = new Dictionary<string, bool>( );
+			Dictionary<string, bool> groupFullControlDeny = new Dictionary<string, bool>( );
 
-			foreach(string group in groups) {
-				foreach(AclEntry entry in entries) {
+			foreach ( string group in groups )
+			{
+				foreach ( AclEntry entry in entries )
+				{
 
-					if(entry.Resource == resource && entry.Subject == group) {
-						if(!groupFullControlGrant.ContainsKey(group)) {
-							groupFullControlGrant.Add(group, false);
-							groupExplicitGrant.Add(group, false);
-							groupFullControlDeny.Add(group, false);
+					if ( entry.Resource == resource && entry.Subject == group )
+					{
+						if ( !groupFullControlGrant.ContainsKey( group ) )
+						{
+							groupFullControlGrant.Add( group, false );
+							groupExplicitGrant.Add( group, false );
+							groupFullControlDeny.Add( group, false );
 						}
 
-						if(entry.Action == action) {
+						if ( entry.Action == action )
+						{
 							// Explicit action
-							if(entry.Value == Value.Grant) {
+							if ( entry.Value == Value.Grant )
+							{
 								// An explicit grant only wins if there are no other explicit deny
-								groupExplicitGrant[group] = true;
+								groupExplicitGrant[ group ] = true;
 							}
-							else if(entry.Value == Value.Deny) {
+							else if ( entry.Value == Value.Deny )
+							{
 								// An explicit deny wins over all other entries
 								return Authorization.Denied;
 							}
 						}
-						else if(entry.Action == AclEntry.FullControlAction) {
+						else if ( entry.Action == AclEntry.FullControlAction )
+						{
 							// Full control, lower priority
-							if(entry.Value == Value.Deny) {
-								groupFullControlDeny[group] = true;
+							if ( entry.Value == Value.Deny )
+							{
+								groupFullControlDeny[ group ] = true;
 							}
-							else if(entry.Value == Value.Grant) {
-								groupFullControlGrant[group] = true;
+							else if ( entry.Value == Value.Grant )
+							{
+								groupFullControlGrant[ group ] = true;
 							}
 						}
 					}
@@ -95,14 +121,20 @@ namespace ScrewTurn.Wiki.AclEngine {
 			// Any explicit grant found at this step wins, because all explicit deny have been processed previously
 			bool tentativeGrant = false;
 			bool tentativeDeny = false;
-			foreach(string group in groupFullControlGrant.Keys) {
-				if(groupExplicitGrant[group]) return Authorization.Granted;
-				
-				if(groupFullControlGrant[group] && !groupFullControlDeny[group]) tentativeGrant = true;
-				if(!groupFullControlGrant[group] && groupFullControlDeny[group]) tentativeDeny = true;
+			foreach ( string group in groupFullControlGrant.Keys )
+			{
+				if ( groupExplicitGrant[ group ] )
+					return Authorization.Granted;
+
+				if ( groupFullControlGrant[ group ] && !groupFullControlDeny[ group ] )
+					tentativeGrant = true;
+				if ( !groupFullControlGrant[ group ] && groupFullControlDeny[ group ] )
+					tentativeDeny = true;
 			}
-			if(tentativeGrant && !tentativeDeny) return Authorization.Granted;
-			if(tentativeDeny) return Authorization.Denied;
+			if ( tentativeGrant && !tentativeDeny )
+				return Authorization.Granted;
+			if ( tentativeDeny )
+				return Authorization.Denied;
 			return Authorization.Unknown;
 		}
 
@@ -111,7 +143,8 @@ namespace ScrewTurn.Wiki.AclEngine {
 	/// <summary>
 	/// Lists legal authorization values.
 	/// </summary>
-	public enum Authorization {
+	public enum Authorization
+	{
 		/// <summary>
 		/// Authorization granted.
 		/// </summary>
